5a6b9f7fe60c18cf85f849b101dcf01194fea291
aish8la <64914670+aish8la@users.noreply.github.com.>
2025-03-09 03:32:15 +0500

    Add test cases

--- src/main.js
+++ src/main.js
@@ -1,24 +1,41 @@
 import { HashMap } from "./hash-map.js";
 import { KeyedLinkedList } from "./keyed-linked-list.js";
 
-const hashMap = new HashMap(KeyedLinkedList);
+const test = new HashMap(KeyedLinkedList);
 
-hashMap.test();
-console.log("count :", hashMap.length());
-console.log(hashMap.hash("dog"));
-hashMap.set("dog", "carlos");
-hashMap.set("cat", "megacarlos");
-console.log("has : ", hashMap.has("dog"));
-hashMap.set("dog", "newcarlos");
-console.log("has : ", hashMap.has("dog"));
-console.log("remove moon :", hashMap.remove("moon"));
-hashMap.test();
-console.log("get : ", hashMap.get("dog"));
-console.log("count :", hashMap.length());
-console.log(hashMap.entries());
-hashMap.clear();
-console.log(hashMap.entries());
-// hashMap.test();
-// console.log("count :", hashMap.length());
+// test.test();
+// console.log("count :", test.length());
+// console.log(test.hash("dog"));
+// test.set("dog", "carlos");
+// test.set("cat", "megacarlos");
+// console.log("has : ", test.has("dog"));
+// test.set("dog", "newcarlos");
+// console.log("has : ", test.has("dog"));
+// console.log("remove moon :", test.remove("moon"));
+// test.test();
+// console.log("get : ", test.get("dog"));
+// console.log("count :", test.length());
+// console.log(test.entries());
+// test.clear();
+// console.log(test.entries());
+// // test.test();
+// // console.log("count :", test.length());
+
+test.set('apple', 'red')
+test.set('banana', 'yellow')
+test.set('carrot', 'orange')
+test.set('dog', 'brown')
+test.set('elephant', 'gray')
+test.set('frog', 'green')
+test.set('grape', 'purple')
+test.set('hat', 'black')
+test.set('ice cream', 'white')
+test.set('jacket', 'blue')
+test.set('kite', 'pink')
+test.set('lion', 'golden')
+
+test.test();
+console.log("length :", test.length())
+
 
 
