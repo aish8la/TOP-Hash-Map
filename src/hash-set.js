8d0f6aa7f10b1f1974200c130246d470c7963cc2
aish8la <64914670+aish8la@users.noreply.github.com.>
2025-03-09 23:34:33 +0500

    Add HashSet class functionality Ported from HashMap functionality to only record key and no value

--- src/hash-set.js
+++ src/hash-set.js
@@ -1,3 +1,131 @@
 export class HashSet {
 
+    constructor(linkedListClass) {
+        this.#linkedListClass = linkedListClass;
+        this.#hashMap = this.createHashMapArr();
+    }
+
+    #linkedListClass;
+    #capacity = 16;
+    #loadFactor = 0.75;
+    #hashMap;
+
+    get capacity() {
+        return this.#capacity;
+    }
+
+    currentLoad() {
+        return this.length() / this.#capacity;
+    }
+
+    createHashMapArr() {
+        return Array.from( {length : this.#capacity }, () => new this.#linkedListClass());
+    }
+
+    validateKey(key) {
+        if(typeof key !== "string") {
+            throw new Error("Key can only be of type string!");
+        }
+
+        return true;
+    }
+
+    validateIndex(index) {
+        if (index < 0 || index >= this.#capacity) {
+            throw new Error("Trying to access index out of bounds");
+        }
+    }
+
+    hash(key) {
+
+        if (!this.validateKey(key)) return null;
+
+        let hashCode = 0;
+        const primeNumber = 31;
+
+        for(let i = 0; i < key.length; i++) {
+            hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % this.#capacity;
+        }
+
+        return hashCode;
+    }
+
+    bucket(key) {
+
+        const index = this.hash(key);
+        this.validateIndex(index);
+        return this.#hashMap[index]
+    }
+
+    set(key) {
+        const currentBucket = this.bucket(key);
+        const node = currentBucket.findNode(key);
+
+        if (node !== null) {
+            return;
+        }
+        currentBucket.append(key);
+        this.growHashMap();
+    }
+
+    get(key) {
+        const currentBucket = this.bucket(key);
+        const node = currentBucket.findNode(key);
+
+        if(node === null) return null;
+        return node;
+    }
+
+    has(key) {
+        const currentBucket = this.bucket(key);
+        return currentBucket.contains(key);
+    }
+
+    remove(key) {
+        const currentBucket = this.bucket(key);
+        const nodeIndex = currentBucket.find(key);
+        if (nodeIndex === null) return false;
+        currentBucket.removeAt(nodeIndex);
+        return true;
+    }
+
+    length() {
+        let nodeCount = 0;
+        for (let i = 0; i < this.#capacity; i++) {
+            nodeCount += this.#hashMap[i].size();
+        }
+        return nodeCount;
+    }
+
+    entries() {
+        let entriesArray = [];
+        let currentNode;
+        for(let i = 0; i < this.#capacity; i++) {
+            if (this.#hashMap[i].head === null) continue;
+            currentNode = this.#hashMap[i].head;
+            while(currentNode !== null) {
+                entriesArray.push(currentNode.key);
+                currentNode = currentNode.nextNode;
+            }
+        }
+        return entriesArray;
+    }
+
+    clear() {
+        this.#hashMap = this.createHashMapArr();
+    }
+
+    growHashMap() {
+        if (this.#loadFactor >= this.currentLoad()) return;
+        const currentEntries = this.entries();
+        this.#capacity *= 2;
+        this.#hashMap = this.createHashMapArr();
+        currentEntries.forEach(entry => this.set(entry));
+    }
+
+    test() {
+        for(let i = 0; i < this.#capacity; i++) {
+            console.log(`[${i}] : `,this.#hashMap[i].toString());
+        }
+    }
 }
