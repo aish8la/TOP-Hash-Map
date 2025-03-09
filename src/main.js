fd2b5957e6e5b94c636eea7090723e8d28213fa3
aish8la <64914670+aish8la@users.noreply.github.com.>
2025-03-09 23:35:44 +0500

    Add Test Cases

--- src/main.js
+++ src/main.js
@@ -1,63 +1,61 @@
 import { HashMap } from "./hash-map.js";
 import { KeyedLinkedList } from "./keyed-linked-list.js";
+import { HashSet } from "./hash-set.js";
+import { LinkedList } from "./linked-list.js";
 
-const test = new HashMap(KeyedLinkedList);
+const hash = new HashMap(KeyedLinkedList);
+const test = new HashSet(LinkedList);
 
-// test.test();
-// console.log("count :", test.length());
-// console.log(test.hash("dog"));
-// test.set("dog", "carlos");
-// test.set("cat", "megacarlos");
-// console.log("has : ", test.has("dog"));
-// test.set("dog", "newcarlos");
-// console.log("has : ", test.has("dog"));
-// console.log("remove moon :", test.remove("moon"));
-// test.test();
-// console.log("get : ", test.get("dog"));
-// console.log("count :", test.length());
-// console.log(test.entries());
-// test.clear();
-// console.log(test.entries());
-// // test.test();
-// // console.log("count :", test.length());
+// test.set('apple', 'red')
+// test.set('banana', 'yellow')
+// test.set('carrot', 'orange')
+// test.set('dog', 'brown')
+// test.set('elephant', 'gray')
+// test.set('frog', 'green')
+// test.set('grape', 'purple')
+// test.set('hat', 'black')
+// test.set('ice cream', 'white')
+// test.set('jacket', 'blue')
+// test.set('kite', 'pink')
+// test.set('lion', 'golden')
 
-test.set('apple', 'red')
-test.set('banana', 'yellow')
-test.set('carrot', 'orange')
-test.set('dog', 'brown')
-test.set('elephant', 'gray')
-test.set('frog', 'green')
-test.set('grape', 'purple')
-test.set('hat', 'black')
-test.set('ice cream', 'white')
-test.set('jacket', 'blue')
-test.set('kite', 'pink')
-test.set('lion', 'golden')
+// console.log("length :", test.length())
+// console.log("capacity :", test.capacity)
+// console.log("current Load :",test.currentLoad());
 
-test.test();
+// test.set('newKey', 'New Value')
+
+// console.log("length :", test.length())
+// console.log("capacity :", test.capacity)
+// console.log("current Load :",test.currentLoad());
+
+// console.log("entries :", test.entries())
+
+// console.log("length :", test.length())
+// console.log("capacity :", test.capacity)
+// console.log("current Load :",test.currentLoad());
+
+
+
+test.set('apple')
+test.set('banana')
+test.set('carrot')
+test.set('dog')
+test.set('elephant')
+test.set('frog')
+test.set('grape')
+test.set('hat',)
+test.set('ice cream')
+test.set('jacket')
+test.set('kite')
+test.set('lion')
+
 console.log("length :", test.length())
 console.log("capacity :", test.capacity)
 console.log("current Load :",test.currentLoad());
 
-test.set('lion', 'black')
-test.set('ice cream', 'blue')
-test.set('jacket', 'green')
+test.set('newKey')
 
-test.test();
-console.log("length :", test.length())
-console.log("capacity :", test.capacity)
-console.log("current Load :",test.currentLoad());
-
-test.set('newKey', 'New Value')
-
-test.test();
-console.log("length :", test.length())
-console.log("capacity :", test.capacity)
-console.log("current Load :",test.currentLoad());
-
-test.set('newKey', 'Override')
-
-test.test();
 console.log("length :", test.length())
 console.log("capacity :", test.capacity)
 console.log("current Load :",test.currentLoad());
