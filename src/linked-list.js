cc29abfa6d4eed344453bfec78dc9a11e9a22505
aish8la <64914670+aish8la@users.noreply.github.com.>
2025-03-09 22:46:24 +0500

    Add LinkedList child class A linked list that only contains a key and no value

---
+++ src/linked-list.js
@@ -0,0 +1,13 @@
+import { KeyedLinkedList } from "./keyed-linked-list";
+
+class Node {
+    constructor(key = null, nextNode = null) {
+      this.key = key;
+      this.nextNode = nextNode;
+    }
+}
+
+export class LinkedList {
+
+
+}
